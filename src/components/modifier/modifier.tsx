3390b82ae9e16ed4916f51a77a97ead7abc624dc
pauliescanlon <pauliescanlon@gmail.com>
2021-02-22 23:51:04 +0000

    feat: refactor modifier component

--- src/components/modifier/modifier.tsx
+++ src/components/modifier/modifier.tsx
@@ -1,8 +1,169 @@
-import React, { FunctionComponent } from 'react'
-import { Box } from 'theme-ui'
+import React, { ChangeEvent, FunctionComponent, useEffect, useState } from 'react'
+import { Box, Grid, Label, Slider } from 'theme-ui'
+import { useTags } from '../../hooks/useTags'
+import { ITagItem } from '../../types'
+import { Svg } from '../svg'
 
-interface IModifierProps {}
+const ROTATE = 'rotate'
+const ROTATE_DEFAULT_VALUE = 0
+const SCALE_DEFAULT_VALUE = 100
 
-export const Modifier: FunctionComponent<IModifierProps> = () => {
-  return <Box>Modifier</Box>
+interface IModifierProps {
+  /** svgAttributes */
+  svgAttributes: {
+    title: string
+    path: string
+  }
+  /** tags from the page */
+  tags: {
+    nodes: ITagItem[]
+  }
+}
+
+interface ICssTransforms {
+  type: string
+  css: string
+  // eslint-disable-next-line
+  values: any
+  // values: ITransformValues
+}
+
+interface ITransformValues {
+  [key: string]: ITransformShape
+}
+
+interface ITransformShape {
+  name: string
+  value: number
+}
+
+const sliderConfig = (name: string) => {
+  return name === ROTATE
+    ? {
+        min: 0,
+        max: 360,
+      }
+    : {
+        min: 10,
+        max: 100,
+      }
+}
+
+const cssConfig = (cssTransforms: ITransformValues) => {
+  return Object.entries(cssTransforms.values)
+    .map((item) => item[1])
+    .reduce((items, item) => {
+      const { name, value } = item as ITransformShape
+      ;(items as string[]).push(name === ROTATE ? `${name}(${value}deg)` : `${name}(${(value / 100).toFixed(1)})`)
+      return items
+    }, [])
+    .toString()
+    .split(',')
+    .join(' ')
+}
+
+export const Modifier: FunctionComponent<IModifierProps> = ({ svgAttributes, tags }) => {
+  const [cssTransforms, setCssTransform] = useState<ICssTransforms>({ type: '', css: null, values: null })
+
+  const tagItems = useTags()
+
+  useEffect(() => {
+    const initialTransforms = tagItems.reduce((items, item) => {
+      const { name } = item.node
+      items[item.node.name] = {
+        name: name,
+        value: name === ROTATE ? ROTATE_DEFAULT_VALUE : SCALE_DEFAULT_VALUE,
+      }
+      return items
+    }, {})
+
+    setCssTransform({
+      type: svgAttributes.title,
+      css: cssConfig({ values: initialTransforms }),
+      values: {
+        ...initialTransforms,
+      },
+    })
+  }, [])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value, name },
+    } = event
+
+    setCssTransform({
+      ...cssTransforms,
+      css: cssConfig({ values: cssTransforms.values }),
+      values: {
+        ...cssTransforms.values,
+        [name]: {
+          name: name,
+          value: value,
+        },
+      },
+    })
+  }
+
+  return (
+    <Box>
+      {/* <Box>
+          <pre>{JSON.stringify(cssTransforms, null, 2)}</pre>
+        </Box> */}
+
+      {cssTransforms.values ? (
+        <Grid
+          sx={{
+            gap: 7,
+            gridTemplateColumns: ['1fr', '1fr 300px'],
+            mb: 7,
+          }}
+        >
+          <Grid
+            sx={{
+              rowGap: 4,
+            }}
+          >
+            {tagItems.map((item, index: number) => {
+              const { name } = item.node
+
+              const isDisabled = tags.nodes.every((val) => val.name !== name)
+
+              return (
+                <Grid
+                  key={index}
+                  sx={{
+                    rowGap: 1,
+                  }}
+                >
+                  <Label
+                    htmlFor={name}
+                    sx={{
+                      color: isDisabled ? 'midGrey' : 'inherit',
+                    }}
+                  >
+                    {name}
+                  </Label>
+                  <Slider
+                    name={name}
+                    disabled={isDisabled}
+                    value={cssTransforms.values[name].value}
+                    onChange={handleChange}
+                    {...sliderConfig(name)}
+                  />
+                </Grid>
+              )
+            })}
+          </Grid>
+          <Svg
+            {...svgAttributes}
+            sx={{
+              color: 'primary',
+              transformOrigin: 'center center',
+              transform: cssTransforms.css,
+            }}
+          />
+        </Grid>
+      ) : null}
+    </Box>
+  )
 }
--- src/templates/wp-post-template.tsx
+++ src/templates/wp-post-template.tsx
@@ -1,107 +1,19 @@
 import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import parse from 'html-react-parser'
-import React, { ChangeEvent, FunctionComponent, useEffect, useState } from 'react'
-import { Box, Container, Grid, Heading, Label, Slider } from 'theme-ui'
-import { Svg } from '../components/svg'
-import { useTags } from '../hooks/useTags'
+import React, { FunctionComponent } from 'react'
+import { Box, Container, Heading } from 'theme-ui'
+import { Modifier } from '../components/modifier'
 import { IPage } from '../types'
-
-const ROTATE = 'rotate'
-const ROTATE_DEFAULT_VALUE = 0
-const SCALE_DEFAULT_VALUE = 100
 
 interface IPostTemplate extends IPage {}
 
-const sliderConfig = (name: string) => {
-  return name === ROTATE
-    ? {
-        min: 0,
-        max: 360,
-      }
-    : {
-        min: 10,
-        max: 100,
-      }
-}
-
-interface ICssTransforms {
-  type: string
-  css: string
-  // eslint-disable-next-line
-  values: any
-  // values: ITransformValues
-}
-
-interface ITransformValues {
-  [key: string]: ITransformShape
-}
-interface ITransformShape {
-  name: string
-  value: number
-}
-
-const cssConfig = (cssTransforms: ITransformValues) => {
-  return Object.entries(cssTransforms)
-    .map((item) => item[1])
-    .reduce((items, item) => {
-      const { name, value } = item as ITransformShape
-      ;(items as string[]).push(name === ROTATE ? `${name}(${value}deg)` : `${name}(${(value / 100).toFixed(1)})`)
-      return items
-    }, [])
-
-    .toString()
-    .split(',')
-    .join(' ')
-}
-
 const WpPostTemplate: FunctionComponent<IPostTemplate> = ({ data: { page } }) => {
-  const [cssTransforms, setCssTransform] = useState<ICssTransforms>({ type: '', css: null, values: null })
-
-  const tagItems = useTags()
-
   const { title, content, tags, featuredImage, svgAttributes } = page
 
   const {
     node: { altText, localFile },
   } = featuredImage || { node: { altText: '', localFile: null } }
-
-  useEffect(() => {
-    const initialTransforms = tagItems.reduce((items, item) => {
-      const { name } = item.node
-      items[item.node.name] = {
-        name: name,
-        value: name === ROTATE ? ROTATE_DEFAULT_VALUE : SCALE_DEFAULT_VALUE,
-      }
-      return items
-    }, {})
-
-    setCssTransform({
-      type: svgAttributes.title,
-      css: cssConfig({ values: initialTransforms }),
-      values: {
-        ...initialTransforms,
-      },
-    })
-  }, [])
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value, name },
-    } = event
-
-    setCssTransform({
-      ...cssTransforms,
-      css: cssConfig({ values: cssTransforms.values }),
-      values: {
-        ...cssTransforms.values,
-        [name]: {
-          name: name,
-          value: value,
-        },
-      },
-    })
-  }
 
   return (
     <Box>
@@ -117,64 +29,7 @@ const WpPostTemplate: FunctionComponent<IPostTemplate> = ({ data: { page } }) =>
           {title}
         </Heading>
 
-        {/* <Box>
-          <pre>{JSON.stringify(cssTransforms, null, 2)}</pre>
-        </Box> */}
-
-        {cssTransforms.values ? (
-          <Grid
-            sx={{
-              gap: 7,
-              gridTemplateColumns: ['1fr', '1fr 300px'],
-              mb: 7,
-            }}
-          >
-            <Grid
-              sx={{
-                rowGap: 4,
-              }}
-            >
-              {tagItems.map((item, index: number) => {
-                const { name } = item.node
-
-                const isDisabled = tags.nodes.every((val) => val.name !== name)
-
-                return (
-                  <Grid
-                    key={index}
-                    sx={{
-                      rowGap: 1,
-                    }}
-                  >
-                    <Label
-                      htmlFor={name}
-                      sx={{
-                        color: isDisabled ? 'midGrey' : 'inherit',
-                      }}
-                    >
-                      {name}
-                    </Label>
-                    <Slider
-                      name={name}
-                      disabled={isDisabled}
-                      value={cssTransforms.values[name].value}
-                      onChange={handleChange}
-                      {...sliderConfig(name)}
-                    />
-                  </Grid>
-                )
-              })}
-            </Grid>
-            <Svg
-              {...svgAttributes}
-              sx={{
-                color: 'primary',
-                transformOrigin: 'center center',
-                transform: cssTransforms.css,
-              }}
-            />
-          </Grid>
-        ) : null}
+        <Modifier svgAttributes={svgAttributes} tags={tags} />
 
         <Box>{parse(content)}</Box>
       </Container>
