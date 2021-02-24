a10fedbdec16a06b56a477f440492be234be5ed9
pauliescanlon <pauliescanlon@gmail.com>
2021-02-24 20:40:15 +0000

    chore: add audio debug

--- src/components/experiment/experiment.tsx
+++ src/components/experiment/experiment.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useEffect, useState } from 'react'
-import { Grid } from 'theme-ui'
+import { Box, Grid, Text } from 'theme-ui'
 import { useMpegs } from '../../hooks/useMpegs'
 import { ISoundItem } from '../../types'
 import { getRandomInt } from '../../utils'
@@ -19,22 +19,46 @@ export const Experiment: FunctionComponent = () => {
   return (
     <Grid
       sx={{
-        gap: '0px',
-        gridTemplateColumns: ['1fr 1fr 1fr', '1fr 1fr 1fr 1fr 1fr'],
+        gap: 6,
       }}
     >
-      {mpegItems.map((item: ISoundItem, index: number) => {
-        const { mediaItemUrl } = item.node
+      <Grid
+        sx={{
+          gap: '0px',
+          gridTemplateColumns: ['1fr 1fr 1fr', '1fr 1fr 1fr 1fr 1fr'],
+        }}
+      >
+        {mpegItems.map((item: ISoundItem, index: number) => {
+          const { mediaItemUrl } = item.node
 
-        return (
-          <SoundTile
-            key={index}
-            index={index}
-            mediaItemUrl={mediaItemUrl}
-            forcePlay={randNum === index ? true : false}
-          />
-        )
-      })}
+          return (
+            <SoundTile
+              key={index}
+              index={index}
+              mediaItemUrl={mediaItemUrl}
+              forcePlay={randNum === index ? true : false}
+            />
+          )
+        })}
+      </Grid>
+      <Box>
+        <Text>Debug</Text>
+        <Box as="ul">
+          {mpegItems.map((item: ISoundItem, index: number) => {
+            const { mediaItemUrl, mimeType } = item.node
+
+            return (
+              <Box as="li" key={index} sx={{ fontSize: '8px' }}>
+                <Text>{mediaItemUrl}</Text>
+                <audio controls>
+                  <source src={mediaItemUrl} type={mimeType} />
+                  Your browser does not support the audio element.
+                </audio>
+              </Box>
+            )
+          })}
+        </Box>
+      </Box>
     </Grid>
   )
 }
