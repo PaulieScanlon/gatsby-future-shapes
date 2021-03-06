df5c1cb5e54a3d08acdfb355be5e9d7427a36ded
pauliescanlon <pauliescanlon@gmail.com>
2021-03-06 18:34:54 +0000

    chore: add focus to slider

--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -256,6 +256,9 @@ export default {
     slider: {
       color: 'primary',
       backgroundColor: 'primaryLight',
+      ':focus': {
+        boxShadow: 'focus',
+      },
       ':disabled': {
         cursor: 'not-allowed',
         color: 'midGrey',
