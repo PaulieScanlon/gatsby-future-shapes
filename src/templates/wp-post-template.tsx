e35688866008c11afe4c583e43bb0db493d35705
pauliescanlon <pauliescanlon@gmail.com>
2021-03-07 09:35:38 +0000

    feat: up image quality for featuredImage

--- src/templates/wp-post-template.tsx
+++ src/templates/wp-post-template.tsx
@@ -89,7 +89,7 @@ export const query = graphql`
           altText
           localFile {
             childImageSharp {
-              gatsbyImageData(quality: 80, layout: FULL_WIDTH, placeholder: DOMINANT_COLOR, formats: [AUTO, WEBP, AVIF])
+              gatsbyImageData(quality: 95, layout: FULL_WIDTH, placeholder: DOMINANT_COLOR, formats: [AUTO, WEBP, AVIF])
             }
           }
         }
